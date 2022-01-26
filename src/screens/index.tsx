53eab0d1f404bfe4c5b2f6a54426572be8bb497d
Marcus Pontes <marcusporama@gmail.com>
2022-01-26 15:15:04 -0300

    configuração app

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { SeachResult } from '../components/SearchResult';
 
 export function Home(){
+    const [name, setName] = useState('')
+
+    function handleSearch(){ 
+        
+    }
+
     return(
-        <View>
-            <Text>Teste</Text>
+        <View style={styles.container}>
+            <Text>Amigos</Text>
+
+            <TextInput 
+                style={styles.input}
+                placeholder="Nome do cliente"
+                onChangeText={setName}
+            />
+
+            <Button
+                title="Buscar" 
+                onPress={handleSearch} 
+            />
+
+            <SeachResult data={[]} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 100,
+        padding: 25,
+    },
+    input: {
+        borderWidth: 1,
+        padding: 7,
+        marginBottom: 10
+    }
+})
